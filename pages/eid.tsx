b2fedb9f6e0d054e7fffa9b5a7b468ec2883d702
Majed Alhasin <sidrozzg@gmail.com>
2022-04-19 05:28:11 +0300

    Changing image download lib

--- pages/eid.tsx
+++ pages/eid.tsx
@@ -1,13 +1,40 @@
 import Link from 'next/link'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from 'src/components/Navbar'
 import Footer from 'src/components/Footer'
 import Head from 'next/head'
 import EidImage from 'src/components/EidImage'
+import html2canvas from 'html2canvas';
 
 const Eid = () => {
     const [name, setName] = useState('')
     const componentRef = useRef();
+    const printRef = React.useRef();
+
+    let isMobile: boolean
+    useEffect(() => {
+        isMobile = /iPhone|iPad|iPod|Android/i.test(navigator?.userAgent);
+        console.log(isMobile);
+    }, [])
+
+    const handleDownloadImage = async () => {
+        const element = printRef.current;
+        const canvas = await html2canvas(element);
+
+        const data = canvas.toDataURL('image/jpg');
+        const link = document.createElement('a');
+
+        if (typeof link.download === 'string') {
+            link.href = data;
+            link.download = 'image.jpg';
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            window.open(data);
+        }
+    };
     return (
         <div className='flex flex-col justify-between h-screen'>
             <Head>
@@ -16,7 +43,7 @@ const Eid = () => {
             </Head>
             <div className="flex flex-col lg:flex-col gap-10 items-center justify-center">
                 <Navbar />
-                <div ref={componentRef} className="relative">
+                <div ref={printRef} className="relative">
                     <EidImage />
                     <div className="font-deco duration-300 right-24 absolute inset-0 z-10 flex justify-center items-center text-4xl text-t3">{name && name}</div>
                 </div>
@@ -33,10 +60,7 @@ const Eid = () => {
                             العودة
                         </button>
                     </Link>
-                    <button onClick={async () => {
-                        const { exportComponentAsPNG } = await import('react-component-export-image')
-                        exportComponentAsPNG(componentRef)
-                    }} className='mt-5 bg-t3 hover:bg-t3 hover:bg-opacity-80 text-white  rounded-tl-2xl rounded-tr-2xl rounded-br-2xl p-2.5 shadow-md transition hover:shadow-lg focus:translate-y-1.5'>
+                    <button onClick={handleDownloadImage} className='mt-5 bg-t3 hover:bg-t3 hover:bg-opacity-80 text-white  rounded-tl-2xl rounded-tr-2xl rounded-br-2xl p-2.5 shadow-md transition hover:shadow-lg focus:translate-y-1.5'>
                         تحميل البطاقة
                     </button>
                 </div>
